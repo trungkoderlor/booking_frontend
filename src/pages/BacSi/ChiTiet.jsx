256fd0ab92c4f3ec109d81cb45145cbed7063a2b
Đậu Huy Trung <trung28282803@gmail.com>
2025-03-21 18:29:43 +0700

    add schedule in doctor detail

--- src/pages/BacSi/ChiTiet.jsx
+++ src/pages/BacSi/ChiTiet.jsx
@@ -7,20 +7,21 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarDays, faHouseMedical } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 const cx = classNames.bind(styles);
 function SDatePicker({ onChange }) {
+  const today = new Date().toISOString().split('T')[0]; // YYYY-MM-DD
+  const [selectedDate, setSelectedDate] = useState(today);
+
   const [dates, setDates] = useState([]);
 
   useEffect(() => {
-    // Lấy danh sách 7 ngày tiếp theo
-    const today = new Date();
     const next7Days = [];
-
     for (let i = 0; i < 7; i++) {
       const futureDate = new Date();
-      futureDate.setDate(today.getDate() + i);
+      futureDate.setDate(futureDate.getDate() + i);
       next7Days.push({
-        value: futureDate.toISOString().split('T')[0], // Format YYYY-MM-DD
+        value: futureDate.toISOString().split('T')[0],
         label: futureDate.toLocaleDateString('vi-VN', {
           weekday: 'long',
           day: '2-digit',
@@ -28,13 +29,16 @@ function SDatePicker({ onChange }) {
         }),
       });
     }
-
     setDates(next7Days);
   }, []);
 
+  useEffect(() => {
+    onChange(selectedDate);
+  }, [selectedDate, onChange]);
+
   return (
     <div className={cx('sDatePicker')}>
-      <select onChange={(e) => onChange(e.target.value)}>
+      <select value={selectedDate} onChange={(e) => setSelectedDate(e.target.value)}>
         {dates.map((date) => (
           <option key={date.value} value={date.value}>
             {date.label}
@@ -48,12 +52,15 @@ function SDatePicker({ onChange }) {
 function ChiTiet() {
   const [doctor, setDoctor] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [scehdule, setSchedule] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]);
   const { slug } = useParams();
   useEffect(() => {
     axios
       .get(`http://localhost:3003/api/doctors/${slug}`)
       .then((response) => {
         setDoctor(response.data);
+
         setLoading(false);
       })
       .catch((error) => {
@@ -64,8 +71,29 @@ function ChiTiet() {
   if (loading) {
     return <div>Loading...</div>;
   }
+
   if (!doctor) {
     return <div>Không tìm thấy bác sĩ</div>;
+  }
+  let filteredSchedule = [];
+  if (doctor.schedule.length > 0) {
+    const now = new Date();
+    const currentHour = now.getHours();
+    filteredSchedule = doctor.schedule.filter((item) => {
+      const scheduleDate = item.date.split('T')[0];
+      if (scheduleDate !== selectedDate) return false;
+
+      if (scheduleDate === new Date().toISOString().split('T')[0]) {
+        const scheduleTime = item.timeType.split(' - ')[0];
+        const [scheduleHour, scheduleMinute] = scheduleTime.split(':').map(Number);
+
+        const scheduleTotalMinutes = scheduleHour * 60 + scheduleMinute;
+        const currentTotalMinutes = now.getHours() * 60 + now.getMinutes();
+        return scheduleTotalMinutes > currentTotalMinutes;
+      }
+
+      return true; // Nếu là ngày khác thì lấy hết
+    });
   }
   //lấy ngày hôm nay
   let today = new Date();
@@ -91,74 +119,26 @@ function ChiTiet() {
       <div className={cx('time-table_clinic')}>
         <div className={cx('time')}>
           <div className={cx('select-date')}>
-            <SDatePicker />
+            <SDatePicker onChange={setSelectedDate} />
           </div>
           <div className={cx('title')}>
             <FontAwesomeIcon icon={faCalendarDays} />
             <span>LỊCH KHÁM</span>
           </div>
-          <div className={cx('time-table')}>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
+          {filteredSchedule.length > 0 ? (
+            <div className={cx('time-table')}>
+              {filteredSchedule.map((item) => (
+                <Link to="/" key={item._id} className={cx('time-item')}>
+                  <span>{item.timeType}</span>
+                </Link>
+              ))}
             </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
+          ) : (
+            <div className={cx('empty')}>
+              <p>Lịch khám trống !</p>
+              <p>Vui lòng chọn ngày khác !</p>
             </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-            <div className={cx('time-item')}>
-              <span>07:00 - 08:00</span>
-            </div>
-          </div>
+          )}
         </div>
         <div className={cx('clinic')}>
           <div className={cx('title')}>
